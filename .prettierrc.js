0ea32b4cc9227ce150accb979a2bf46d1ff93239
Guangyuan <254202042@qq.com>
2023-10-06 11:08:17 +0800

    chore: add prettier

---
+++ .prettierrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    // 箭头函数只有一个参数的时候可以忽略括号
+    arrowParens: 'avoid',
+    // 括号内部不要出现空格
+    bracketSpacing: true,
+    // 使用行结束符 ‘lf'
+    endOfLine: 'lf',
+    proseWrap: 'preserve',
+    // 行款
+    printWidth: 100,
+    // 是否使用分号，一般用来放在句子结尾
+    semi: false,
+    // 使用单引号
+    singleQuote: true,
+    // 缩进
+    tabWidth: 2,
+    useTabs: false,
+    trailingComma: 'es5',
+    parser: 'typescript'
+}
